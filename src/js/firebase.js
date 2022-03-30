78c3b74cbf6daaf668be709279874c26f4d0d591
Pato07brn <brunonascimento0507@gmail.com>
2022-03-30 16:46:24 -0300

    Autenticação 70%

--- src/js/firebase.js
+++ src/js/firebase.js
@@ -1,8 +1,9 @@
 import { async } from '@firebase/util';
 import { initializeApp } from 'firebase/app';
 import { getAuth, setPersistence, signInWithEmailAndPassword, browserSessionPersistence, getIdToken } from "firebase/auth";
-import { getDatabase, ref, set } from "firebase/database";
+import { getDatabase, ref, set, child, get } from "firebase/database";
 import { getFirestore, collection, doc, setDoc, addDoc, getDocs, query, where } from 'firebase/firestore';
+import { stubString } from 'lodash';
 import '../css/style.css'
 
 //Configuração
@@ -26,36 +27,67 @@ const db = getFirestore(app);
 //Inicia a autenticacao
 const auth = getAuth(app);
 
-function writeUserData(idToken, email,) {
-  const db = getDatabase();
-  set(ref(db, 'users/' + 'user2'), {
-    id: idToken,
-    email: email,
-  });
-}
-
-function recebeId() {
-  getIdToken(auth.currentUser)
-    .then((idToken) => {
-      return idToken
-    })
-    .catch((error) => {
-      console.log('token não gerado');
-    });
-}
-
 window.autentica = function () {
   const dados = dadosParaAutentica()
   signInWithEmailAndPassword(auth, dados.email, dados.password)
     .then((userCredential) => {
-      const user = userCredential.user;
+      const user = userCredential.user
       alert('usuário logado')
       setPersistence(auth, browserSessionPersistence)
-      window.location.href = 'adicionar-novo.html'
+      window.location.href = 'index.html';
     })
     .catch((error) => {
       alert('Senha ou Email incorretos')
     });
+}
+
+function writeUserData(idToken, id) {
+  const db = getDatabase();
+  set(ref(db, 'users/auth/' + id), {
+    id: idToken,
+  });
+}
+
+function lerIdBanco(idToken) {
+  const dbRef = ref(getDatabase());
+  get(child(dbRef, 'users/auth/'+idToken)).then((snapshot) => {
+    if (snapshot.exists()) {
+      console.log(snapshot.val());
+      return toString(snapshot)
+    } else {
+      console.log("No data available");
+    }
+  }).catch((error) => {
+    console.error(error);
+  });
+}
+
+window.lerIdLocal = function(){
+  getIdToken(auth.currentUser)
+    .then((idToken) => {
+      return idToken;
+    })
+    .catch((error) => {
+      alert('token não gerado, o seu usuário não está logado ' + error);
+    });
+}
+
+window.escreveId = function () {
+  getIdToken(auth.currentUser)
+    .then((idToken) => {
+      writeUserData(idToken, idToken.substring(0, 7))
+    })
+    .catch((error) => {
+      alert('token não gerado, o seu usuário não está logado ' + error);
+    });
+}
+
+
+// Retorna os dados do formulário
+function dadosParaAutentica() {
+  const email = document.getElementById('email').value
+  const password = document.getElementById('pass').value
+  return { email, password }
 }
 
 //Verifica se existe no Firebase
@@ -69,14 +101,6 @@ async function verificaDados(consulta) {
   });
   return valuesBolo.nome
 }
-
-// Retorna os dados do formulário
-function dadosParaAutentica() {
-  const email = document.getElementById('email').value
-  const password = document.getElementById('pass').value
-  return { email, password }
-}
-
 
 // Retorna os dados do formulário
 function dadosParaServ() {
@@ -99,18 +123,21 @@ function dadosParaServ() {
 window.adicionarDados = async function () {
   const dados = dadosParaServ()
   const verifica = await verificaDados(dados.nome);
-  const idDeSessao = recebeId()
+  const idlocal = lerIdLocal()
+  console.log(idlocal);
+  const idBanco = lerIdBanco(id)
   if (dados.nome == verifica) {
     alert("A receita já Existe")
   }
   else {
-    if(idDeSessao == recebeId()){
-    setDoc(doc(db, "Produtos-docs", dados.nome), dados);
-    alert("Receita adicionada ao catálogo com sucesso com sucesso")
+    if (idlocal == idBanco) {
+      setDoc(doc(db, "Produtos-docs", dados.nome), dados);
+      alert("Receita adicionada ao catálogo com sucesso com sucesso")
+      window.location.href = 'index.html'
     }
-    else{
-    alert("Usuário não está logado")
-    window.location.href = 'autenticacao.html'
+    else {
+      alert("Usuário não está logado")
+      window.location.href = 'autenticacao.html'
     }
   }
 }
